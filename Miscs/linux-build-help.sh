13723aa8e76db8b1efa82c837a6e3a12c2ba380c
phan2410 <phan2410@gmail.com>
2021-02-15 23:16:33 +0700

    add helper files

---
+++ Miscs/linux-build-help.sh
@@ -0,0 +1,7 @@
+export PATH=/home/fan/Qt/5.9/gcc_64/bin/:$PATH
+export LD_LIBRARY_PATH=/home/fan/Qt/5.9/gcc_64/lib:$LD_LIBRARY_PATH
+export QT_PLUGIN_PATH=/home/fan/Qt/5.9/gcc_64/plugins:$QT_PLUGIN_PATH
+export QML2_IMPORT_PATH=/home/fan/Qt/5.9/gcc_64/qml:$QML2_IMPORT_PATH
+sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
+sudo apt update
+sudo apt install libjasper1 libjasper-dev
